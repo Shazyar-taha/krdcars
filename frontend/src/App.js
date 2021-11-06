2b7672232e353c9b78811924844ce807216c374d
rawand-faraidun <rawandmessi21@gmail.com>
2021-11-07 01:19:21 +0300

    header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CssBaseline } from '@mui/material'
+import { BrowserRouter as Router } from 'react-router-dom';
 
+import Header from './components/partials/Header/Header';
 import './components/partials/designs/stylesheets/main.scss'
 
 
@@ -9,9 +11,22 @@ import './components/partials/designs/stylesheets/main.scss'
  *  @return {Element} : app frontend
  */
 export default function App() {
+
+    // removing the preload class from the body when the page is ready
+    // preload class prevent anything to animate before the whole page is ready
+    useEffect(() => {
+        document.getElementsByTagName('body')[0].classList.remove('preload')
+    }, [])
+
+
     return (
-        <>
+        <Router>
+
+            {/* material ui css baselines */}
             <CssBaseline />
-        </>
+
+            {/* header of the document */}
+            <Header />
+        </Router>
     );
 }
