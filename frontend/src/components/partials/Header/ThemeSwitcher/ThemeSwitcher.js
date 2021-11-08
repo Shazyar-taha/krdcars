df20d5d211f3eb6d7f484ffc511e6509e043a942
rawand-faraidun <rawandmessi21@gmail.com>
2021-11-08 23:39:05 +0300

    theme switcher component

---
+++ frontend/src/components/partials/Header/ThemeSwitcher/ThemeSwitcher.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import Brightness4Outlined from '@mui/icons-material/Brightness4Outlined'
+
+import './theme-switcher.scss'
+
+
+
+/**
+ *  @return {Element} : theme switcher
+ */
+export default function ThemeSwitcher() {
+
+    // getting user choosen theme color
+    const [themeState, setThemeState] = useState(localStorage.getItem('theme-color'))
+
+    // if the local storage theme was not as expected, making a new one
+    if (themeState !== 'light' && themeState !== 'dark') {
+        setThemeState('light')
+    }
+
+    // adding theme color property to the HTML document
+    document.documentElement.setAttribute('theme-color', themeState)
+
+    // saving the user theme changes
+    localStorage.setItem('theme-color', themeState)
+
+
+    // theme switch toogle
+    function toggleTheme(e) {
+        // if the current theme was light, changing it to dark
+        if (e.target.closest('.theme-switcher').getAttribute('theme') === 'light') {
+            setThemeState('dark')
+        }
+        // changing to light otherwise
+        else if (e.target.closest('.theme-switcher').getAttribute('theme') === 'dark') {
+            setThemeState('light')
+        }
+    }
+
+
+    return (
+        <div className="theme-switcher" onClick={toggleTheme} theme={themeState} >
+            <Brightness4Outlined />
+        </div>
+    )
+}
