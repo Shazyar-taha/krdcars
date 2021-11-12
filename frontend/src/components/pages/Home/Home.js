ff732c1bb998eb1ee06b43fc4e29656739b2136c
rawand-faraidun <rawandmessi21@gmail.com>
2021-11-12 21:55:06 +0300

    home page

--- frontend/src/components/pages/Home/Home.js
+++ frontend/src/components/pages/Home/Home.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
+import './home.scoped.scss'
+import HomeMain from './HomeMain/HomeMain'
+
 
 
 /**
- *  @return {Element} : app home page
+ *  @return {Element} : home page
  */
 export default function Home() {
     return (
         <div className="home-page">
 
-            home
+            {/* main section of homepage */}
+            <HomeMain />
 
         </div>
     )
