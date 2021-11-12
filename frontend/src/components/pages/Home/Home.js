868dfb015712ff5cfd40e3da22c69ce5c19b73d1
rawand-faraidun <rawandmessi21@gmail.com>
2021-11-12 18:50:36 +0300

    home page component

---
+++ frontend/src/components/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+/**
+ *  @return {Element} : app home page
+ */
+export default function Home() {
+    return (
+        <div className="home-page">
+
+            home
+
+        </div>
+    )
+}
