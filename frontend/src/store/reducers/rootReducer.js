bcd80e59f4b1b3be82bc8d98384386df85d64583
rawand-faraidun <rawandmessi21@gmail.com>
2021-11-20 15:22:24 +0300

    language reducer

--- frontend/src/store/reducers/rootReducer.js
+++ frontend/src/store/reducers/rootReducer.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux'
 
+import languageReducer from "./language";
+
 
 
 // Combine all reducers
 const rootReducer = combineReducers({
+    language: languageReducer
 })
 
 
