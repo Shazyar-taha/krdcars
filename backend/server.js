791ff7c0164b4161863455206ea0f45970309180
rawand-faraidun <rawandmessi21@gmail.com>
2021-11-30 19:11:49 +0300

    serving frontend from server

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,31 @@
 require('dotenv').config({ path: './../.env' });
 const express = require('express');
 const routes = require('./routes/routes');
+const path = require('path');
+const cors = require('cors');
 
 
 const app = express();
 
-app.use(express.json({
-    limit: '10mb'
-}));
 
+// allowing request body
+app.use(express.json({ limit: '10mb' }));
+// static files
+app.use(express.static(path.resolve(__dirname, '../frontend/build')))
+// allowing CORS requests
+app.use(cors())
+
+
+
+// serving app frontend
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'))
+});
 
 // route midllwares
 app.use(routes);
 
-app.use((req, res, next) => {
-    res.status(404).send('Not Found');
-});
+
 
 // running server
 app.listen(process.env.PORT || 5000, () => {
