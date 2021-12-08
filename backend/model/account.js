06ceffb78cf4115a135ec1b5721ec1f9bee779ab
Shazyar <p.shazyar108@gmail.com>
2021-12-08 17:23:51 +0300

    added account

--- backend/model/account.js
+++ backend/model/account.js
@@ -7,14 +7,14 @@ const db = require('./db');
 // exist the email then tell the user change the email
 function isExistEmail(email) {
     const sql = "SELECT email FROM account WHERE email = ?";
-    return db.execute(sql, [email]);
+    return db.query(sql, [email]);
 }
 
 
 
 exports.addUser = (user) => {
     let isExist = isExistEmail(user.email).then(([rows, fieldData]) => {
-
+        console.log(rows);
     }).catch((err) => console.log(err));
 
 
