ec11c5894907155e8ccf40cf998943f0aa3d6847
Shazyar <p.shazyar108@gmail.com>
2021-12-18 15:13:47 +0300

    adding change password to account

--- backend/controller/account.js
+++ backend/controller/account.js
@@ -3,18 +3,40 @@ const accountModel = require('../model/account');
 const route = require('express').Router();
 
 
-route.get('/user', async (req, res) => {
+// adding a new user to database
+route.post('/user', async (req, res) => {
 
     const user = {
-        fullName: "Sara Ahmaf",
-        password: "dsfk",
-        email: "ddddd@yahoo.com",
-        permission: "USER",
-        profileImg: "ffffff"
+        fullName: "Shazyar Taha Abdulla",
+        password: "shazyarth12",
+        email: "p.shazyar108@gmail.com",
+        permission: "ADMIN",
+        profileImg: "dsfiuewrim,cvndfiesd"
     }
+
     let isAdded = await accountModel.addUser(user);
     console.log(isAdded);
 });
 
+
+// changing the user password
+route.post('/change-pass', async (req, res) => {
+    let isUpdate = await accountModel.changePassword({
+        id: 1,
+        oldPass: 'shazyar13',
+        newPass: 'shazyar14',
+        confirmPass: 'shazyar14'
+    });
+
+    if (isUpdate) {
+        res.send({
+            message: 'change password'
+        });
+    } else {
+        res.send({
+            message: 'not changed'
+        });
+    }
+})
 
 module.exports = route;
