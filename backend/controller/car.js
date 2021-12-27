5c3e2dfd205691ee70093a971d704e084cb0803c
Shazyar <p.shazyar108@gmail.com>
2021-12-27 17:34:56 +0300

    finish info car

--- backend/controller/car.js
+++ backend/controller/car.js
@@ -5,62 +5,37 @@ const carModel = require('../model/car');
 
 // get car brands
 router.get('/cars/brand', (req, res) => {
-    console.log(res.params.branduid);
     // limit is 20
     const page = parseInt(req.query.page || 1);
     // offset
     let offset = 20 * (page - 1);
 
     // declare two arrays
-    let brands = [],
-        titles = [],
-        objects = [];
+    let brands = [];
 
     let urlId = 0;
 
-
     // getting data from car model 
     carModel.findAllBrand(offset).then(([rows, fieldData]) => {
-        // i wanna create array of the object which object is this
-        // {url: name, value: {en: value, kr: value}, img: value}
-        // so we gotta do this
-        rows.forEach(row => {
 
+        rows.forEach(row => {
             if (row.id != urlId) {
 
                 urlId = row.id;
                 // push the data to brands array
                 brands.push({
-                    id: row.id,
                     url: row.url_name,
+                    title: {
+                        en: rows.find(r => r.id == row.id && r.language_id == 1).title,
+                        kr: rows.find(r => r.id == row.id && r.language_id == 2).title
+                    },
                     img: row.img
                 });
 
             }
-
-            // pushing data to titles array
-            titles.push({
-                url_id: row.id,
-                title: row.title,
-                language_id: row.language_id
-            });
-
         });
 
-        // after that i creating the new array and send it
-        // for each brand
-        brands.forEach(brand => {
-            objects.push({
-                url: brand.url,
-                title: {
-                    en: titles.filter(t => t.url_id == brand.id && t.language_id == 1).map(t => t.title)[0],
-                    kr: titles.filter(t => t.url_id == brand.id && t.language_id == 2).map(t => t.title)[0]
-                },
-                img: brand.img
-            });
-        });
-
-        res.send(objects);
+        res.send(brands);
 
     }).catch((err) => {
         console.log(err);
@@ -69,33 +44,58 @@ router.get('/cars/brand', (req, res) => {
 });
 
 // get specific brands
-router.get('/cars/brand:brandUId', (req, res) => {
+router.get('/cars/brand/:brandUId', (req, res) => {
     const urlName = req.params.brandUId;
+    let models = [];
+    let modelUrl = '';
+    carModel.findBrandByUId(urlName).then(([brandRows, fieldData]) => {
+        if (brandRows.length != 0) {
+            carModel.findAllModelsByBrand(urlName).then(([modelRows, fieldData]) => {
+                // get all models and merge the model name with language
+                modelRows.forEach(model => {
+                    if (modelUrl.length == 0 || modelUrl != model.url_name) {
+                        modelUrl = model.url_name;
 
-    carModel.findBrandByUId(urlName).then(([rows, fieldData]) => {
+                        models.push({
+                            url: model.url_name,
+                            title: {
+                                en: modelRows.filter(r => r.url_name == model.url_name && r.language_id == 1)
+                                    .map(r => r.model_name)[0],
+                                kr: modelRows.filter(r => r.url_name == model.url_name && r.language_id == 2)
+                                    .map(r => r.model_name)[0]
+                            }
 
-        res.send({
-            brand: {
-                name: {
-                    en: rows.filter(row => row.language_id = 1).map(row => row.brand_name)[0],
-                    kr: rows.filter(row => row.language_id = 2).map(row => row.brand_name)[0]
-                },
-                founderName: {
-                    en: rows.filter(row => row.language_id = 1).map(row => row.founder_name)[0],
-                    kr: rows.filter(row => row.language_id = 2).map(row => row.founder_name)[0]
-                },
-                foundDate: rows[0].founder_date,
-                headquartersLocation: {
-                    en: rows.filter(row => row.language_id = 1).map(row => row.headquarters_location)[0],
-                    kr: rows.filter(row => row.language_id = 2).map(row => row.headquarters_location)[0]
-                }
-            },
-            model: {
+                        })
+                    }
+                });
 
-            }
-        });
+                res.send({
+                    brand: {
+                        name: {
+                            en: brandRows.find(row => row.language_id == 1).brand_name,
+                            kr: brandRows.find(row => row.language_id == 2).brand_name
+                        },
+                        founderName: {
+                            en: brandRows.find(row => row.language_id == 1).founder_name,
+                            kr: brandRows.find(row => row.language_id == 2).founder_name
+                        },
+                        foundDate: brandRows[0].founder_date,
+                        headquartersLocation: {
+                            en: brandRows.find(row => row.language_id == 1).headquarters_location,
+                            kr: brandRows.find(row => row.language_id == 2).headquarters_location
+                        }
+                    },
+                    model: models
 
 
+                });
+
+            });
+        } else {
+            res.send({
+                message: 'Sorry that brand is not available'
+            })
+        }
     }).catch((err) => {
         console.log(err);
     });
@@ -103,46 +103,90 @@ router.get('/cars/brand:brandUId', (req, res) => {
 });
 
 
+// get specific car 
+router.get('/cars/brand/:brandUid/:modelUid', (req, res) => {
+    const brandUid = req.params.brandUid,
+        modelUid = req.params.modelUid;
 
+    const year = parseInt(req.query.year || 0);
 
-// get car models
-router.get('/model', (req, res) => {
-    carModel.models(1).then(([rows, fieldData]) => {
-        res.send(rows);
-    }).catch((err) => {
-        console.log(err);
-    })
+    console.log('the year is ' + year);
+
+    if (year == 0) {
+        carModel.findCarByModelByBrand(brandUid, modelUid)
+            .then(([rows, fieldData]) => {
+                if (rows.length != 0) {
+                    carModel.findCarYearByModelByBrand(brandUid, modelUid).
+                    then(([modelYears, fieldData]) => {
+
+                        res.send({
+                            name: {
+                                en: rows.find(r => r.language_id == 1).car_name,
+                                kr: rows.find(r => r.language_id == 2).car_name
+                            },
+                            carInformation: {
+                                en: rows.find(r => r.language_id == 1).car_information,
+                                kr: rows.find(r => r.language_id == 2).car_information
+                            },
+                            img: rows.find(r => r.language_id == 1).img,
+                            carType: {
+                                en: rows.find(r => r.language_id == 1).car_type_name,
+                                kr: rows.find(r => r.language_id == 2).car_type_name
+                            },
+                            availableYears: modelYears.map(r => r.car_year)
+
+                        });
+                    }).catch(err => {
+                        console.log(err);
+                    });
+                } else {
+                    res.send({
+                        message: 'Sorry that brand with that model is not available'
+                    });
+                }
+            }).catch((err) => {
+                console.log(err);
+            });
+    } else {
+        carModel.findCarByYear(brandUid, modelUid, year)
+            .then(([rows, fieldData]) => {
+                if (rows.length != 0) {
+                    carModel.findCarYearByModelByBrand(brandUid, modelUid).
+                    then(([modelYears, fieldData]) => {
+
+                        res.send({
+                            name: {
+                                en: rows.find(r => r.language_id == 1).car_name,
+                                kr: rows.find(r => r.language_id == 2).car_name
+                            },
+                            carInformation: {
+                                en: rows.find(r => r.language_id == 1).car_information,
+                                kr: rows.find(r => r.language_id == 2).car_information
+                            },
+                            img: rows.find(r => r.language_id == 1).img,
+                            carType: {
+                                en: rows.find(r => r.language_id == 1).car_type_name,
+                                kr: rows.find(r => r.language_id == 2).car_type_name
+                            },
+                            availableYears: modelYears.map(r => r.car_year)
+
+                        });
+                    }).catch(err => {
+                        console.log(err);
+                    });
+                } else {
+                    res.send({
+                        message: 'Please Check the model or year'
+                    });
+                }
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
+
 })
 
 
-// get cars by model id
-router.get('/car-models', (req, res) => {
-    const modelId = 1;
-
-    carModel.findCarByModel({
-        languageId: 1,
-        modelId: modelId
-    }).then(([rows, fieldData]) => {
-        res.send(rows);
-    }).catch((err) => {
-        console.log(err);
-    });
-
-
-})
-
-// get car by car id
-router.get('/car', (req, res) => {
-    const carId = 1;
-    carModel.findCarById({
-        languageId: 1,
-        carId: 1
-    }).then(([rows, fieldData]) => {
-        res.send(rows);
-    }).catch((err) => {
-        console.log(err);
-    });
-})
 
 
 module.exports = router;
--- backend/model/car.js
+++ backend/model/car.js
@@ -2,8 +2,6 @@ const db = require('./db');
 
 
 
-
-
 // fetch all brands from brand table 
 exports.findAllBrand = (offset) => {
     const sql = `SELECT 
@@ -39,65 +37,108 @@ exports.findBrandByUId = (uid) => {
     return db.query(sql, [uid]);
 }
 
-// fetch all models from model table
-exports.models = (languageId) => {
-    const sql = `SELECT
-                id, 
-                model_name
-                        FROM
-                model
-            WHERE language_id = ?`;
-    return db.execute(sql, [languageId]);
 
+// fetch the models using brand url name
+exports.findAllModelsByBrand = (brandUId) => {
+    const sql = `SELECT 
+                    u_model.name AS url_name,
+                    m.model_name,
+                    m.language_id
+                FROM 
+                    car 
+                INNER JOIN 
+                    model m ON m.id = car.model_id
+                INNER JOIN 
+                    url u_model ON u_model.id = m.url_id
+                INNER JOIN 
+                    brand b ON b.id = car.brand_id
+                INNER JOIN 
+                    url u_brand ON u_brand.id = b.url_id 
+                WHERE 
+                    u_brand.name = ?
+                ORDER BY u_model.name, m.language_id ASC`;
+    return db.query(sql, [brandUId]);
 }
 
 
-// fetch cars by model
-// the parameters contain languageId and modelId
-exports.findCarByModel = (params) => {
+// fetch the all models of a specific brand
+exports.findCarByModelByBrand = (brandUid, modelUid) => {
     const sql = `SELECT 
-    car.id,
-	car.car_name,
-    br.brand_name,
-	m.model_name,
-    ct.car_type_name,
-    car.car_information,
-    car.img,
-    car.car_year
-FROM
-	car
-INNER JOIN 
-	brand br ON br.id = car.brand_id
-INNER JOIN
-	model m ON m.id = car.model_id
-INNER JOIN	
-	car_type ct ON ct.id = car.car_type_id
-WHERE 
-	language_id = ? AND m.id = ?`;
-    return db.execute(sql, [params.languageId, params.modelId]);
+                    c.car_name,
+                    c.car_information,
+                    c.img, 
+                    ct.car_type_name,
+                    c.language_id, 
+                    c.img,
+                    MAX(c.car_year) AS car_year
+                    
+                FROM
+                    car c
+                INNER JOIN
+                    car_type ct ON ct.id = c.car_type_id
+                        AND ct.language_id = c.language_id
+                INNER JOIN 
+                    brand br ON br.id = c.brand_id
+                INNER JOIN 
+                    model m ON m.id = c.model_id
+                INNER JOIN 
+                    url u_brand ON u_brand.id = br.url_id
+                INNER JOIN 
+                    url u_model ON u_model.id = m.url_id
+                WHERE
+                    (u_brand.name = ? AND u_model.name = ?)
+                GROUP BY
+                    c.car_name`;
+
+    return db.query(sql, [brandUid, modelUid]);
 }
 
-// fetch a car by id
-// the parameters contains languageId and carId
-exports.findCarById = (params) => {
+// fetch all years of the specific model and brand
+exports.findCarYearByModelByBrand = (brandUid, modelUid) => {
+    const sql = `SELECT DISTINCT
+                    c.car_year
+                FROM
+                    car c
+                        INNER JOIN
+                    brand br ON br.id = c.brand_id
+                        INNER JOIN
+                    model m ON m.id = c.model_id
+                        INNER JOIN
+                    url u_brand ON u_brand.id = br.url_id
+                        INNER JOIN
+                    url u_model ON u_model.id = m.url_id
+                WHERE
+                    (u_brand.name = ?
+                        AND u_model.name = ?);`;
+    return db.query(sql, [brandUid, modelUid]);
+}
+
+
+// fetch a car by year, model and brand
+exports.findCarByYear = (brandUid, modelUid, year) => {
     const sql = `SELECT 
-    car.id,
-	car.car_name,
-    br.brand_name,
-	m.model_name,
-    ct.car_type_name,
-    car.car_information,
-    car.img,
-    car.car_year
-FROM
-	car
-INNER JOIN 
-	brand br ON br.id = car.brand_id
-INNER JOIN
-	model m ON m.id = car.model_id
-INNER JOIN	
-	car_type ct ON ct.id = car.car_type_id
-WHERE 
-	language_id = ? AND m.id = ?`;
-    return db.execute(sql, [params.languageId, params.carId]);
+                c.car_name,
+                c.car_information,
+                c.img, 
+                ct.car_type_name,
+                c.language_id, 
+                c.img
+            FROM
+                car c
+            INNER JOIN
+                car_type ct ON ct.id = c.car_type_id
+                    AND ct.language_id = c.language_id
+            INNER JOIN 
+                brand br ON br.id = c.brand_id
+            INNER JOIN 
+                model m ON m.id = c.model_id
+            INNER JOIN 
+                url u_brand ON u_brand.id = br.url_id
+            INNER JOIN 
+                url u_model ON u_model.id = m.url_id
+            WHERE
+                (u_brand.name = ? AND u_model.name = ?)
+                AND c.car_year = ?`;
+    return db.query(sql, [brandUid, modelUid, year]);
+
 }
