6f80c1895890e6db6ea80edf2986430270242521
rawand-faraidun <rawandmessi21@gmail.com>
2022-01-05 17:51:40 +0300

    fetching brands list

--- frontend/src/components/pages/Info/InfoCars/InfoCars.js
+++ frontend/src/components/pages/Info/InfoCars/InfoCars.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useRouteMatch } from 'react-router'
 import { Container } from '@mui/material'
+import axios from 'axios'
 
 import PageTitle from '../../../partials/helpers/PageTitle/PageTitle'
 import { CenteredGrid } from '../../../partials/helpers/PageGrid/PageGrid'
@@ -28,28 +29,18 @@ export default function InfoCars() {
     const { url } = useRouteMatch()
 
 
-    // brands list state
+    // brands list
     const [datas, setDatas] = useState([]);
 
     // get the datas list
     useEffect(() => {
-        /**
-        * @TODO : fetch from server
-        */
-        const response = [
-            {
-                url: 'mitsubishi',
-                title: { en: 'Mitsubishi', kr: 'میسۆبیشی' },
-                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Mitsubishi_logo.svg/2381px-Mitsubishi_logo.svg.png'
-            },
-            {
-                url: 'maseraty',
-                title: { en: 'Maseraty', kr: 'ماسێراتی' },
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcgsvwop0SIxlaP4knSlTeF8M-0MUQu2hc7g&usqp=CAU'
-            },
-        ]
-
-        setDatas(response)
+        axios.get('/apis/info/cars/brand')
+            .then(res => {
+                setDatas(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }, [])
 
 
