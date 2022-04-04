eb6b570ae7719711eba3c4f153ee0874d0361b98
rawand-faraidun <rawandmessi21@gmail.com>
2022-04-04 20:46:40 +0300

    add info post route

--- backend/controller/contact.js
+++ backend/controller/contact.js
@@ -7,12 +7,24 @@ const contact = require('../model/contact');
 
 
 router.get("/get-details", async (req, res) => {
-   
+
     // get all brands
     let [brands, brandsField] = await contact.getBrands();
-    
+
     // get all car type
     let [carTypes, carTypesField] = await contact.getCarType();
+
+    // changing car type name arraysto object 
+    carTypes.forEach(carType => {
+        let name = {}
+        carType.name.forEach(type => name = { ...name, ...type })
+        name.en = name.EN
+        name.kr = name.KR
+        delete name.EN
+        delete name.KR
+        carType.name = name
+        return carType
+    })
 
     // send response
     res.send({
@@ -32,8 +44,24 @@ router.get("/get-models/:brandId", async (req, res) => {
 
     // send response
     res.send(models);
-    
+
 });
+
+
+router.post('/send-contact', async (req, res) => {
+
+    /**
+     * @TODO : post datas from ad information
+     */
+
+    console.log(req.body);
+    console.log(req.user);
+
+
+    res.send({
+        message: 'SUCCESS' || 'FAILED'
+    })
+})
 
 
 module.exports = router;
